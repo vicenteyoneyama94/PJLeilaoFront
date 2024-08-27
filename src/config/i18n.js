e87b2427f06325218c8b64771663caf057c2ea77
Vicente Yoneyama garra <vicenteyoneyama@gmail.com>
2024-08-27 15:01:08 -0300

    correções pasta locales e i18n

--- src/config/i18n.js
+++ src/config/i18n.js
@@ -1,8 +1,10 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import ptBR from './locales/ptBR.json';
-import enUS from './locales/enUS.json';
+import ptBR from '../locales/ptBR.json';
+import enUS from '../locales/enUS.json';
+
+
 
 const resources = {
     en: {
@@ -18,7 +20,7 @@ i18n
     .use(initReactI18next)
     .init({
         resources,
-        fallbackLng: 'en',
+        fallbackLng: 'enUS',
         interpolation: {
             escapeValue: false,
         },
